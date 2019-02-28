174d47b593584c5df5c605a4d05941e71ccec41c
Henery Paina <heneryPaina@gmail.com>
2019-02-28 08:24:02 -0300

    calculadora
    henery paina

---
+++ Calculadora/Form1.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalculadora
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btnLimpar = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btnPonto = New System.Windows.Forms.Button()
+        Me.btnMultiplicar = New System.Windows.Forms.Button()
+        Me.btnDividir = New System.Windows.Forms.Button()
+        Me.btnSubtrair = New System.Windows.Forms.Button()
+        Me.btnSomar = New System.Windows.Forms.Button()
+        Me.txtResultado = New System.Windows.Forms.MaskedTextBox()
+        Me.btnIgual = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn7
+        '
+        Me.btn7.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.Location = New System.Drawing.Point(14, 107)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(50, 30)
+        Me.btn7.TabIndex = 0
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.Location = New System.Drawing.Point(81, 107)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(50, 30)
+        Me.btn8.TabIndex = 0
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.Location = New System.Drawing.Point(150, 107)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(50, 30)
+        Me.btn9.TabIndex = 0
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(14, 146)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(50, 30)
+        Me.btn4.TabIndex = 0
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.Location = New System.Drawing.Point(81, 146)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(50, 30)
+        Me.btn5.TabIndex = 0
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(14, 185)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(50, 30)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(81, 185)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(50, 30)
+        Me.btn2.TabIndex = 0
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btnLimpar
+        '
+        Me.btnLimpar.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLimpar.Location = New System.Drawing.Point(221, 68)
+        Me.btnLimpar.Name = "btnLimpar"
+        Me.btnLimpar.Size = New System.Drawing.Size(50, 30)
+        Me.btnLimpar.TabIndex = 0
+        Me.btnLimpar.Text = "CE"
+        Me.btnLimpar.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn0.Location = New System.Drawing.Point(14, 224)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(50, 30)
+        Me.btn0.TabIndex = 0
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.Location = New System.Drawing.Point(150, 146)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(50, 30)
+        Me.btn6.TabIndex = 0
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(150, 185)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(50, 30)
+        Me.btn3.TabIndex = 0
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btnPonto
+        '
+        Me.btnPonto.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPonto.Location = New System.Drawing.Point(81, 224)
+        Me.btnPonto.Name = "btnPonto"
+        Me.btnPonto.Size = New System.Drawing.Size(50, 30)
+        Me.btnPonto.TabIndex = 0
+        Me.btnPonto.Text = "."
+        Me.btnPonto.UseVisualStyleBackColor = True
+        '
+        'btnMultiplicar
+        '
+        Me.btnMultiplicar.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMultiplicar.Location = New System.Drawing.Point(221, 146)
+        Me.btnMultiplicar.Name = "btnMultiplicar"
+        Me.btnMultiplicar.Size = New System.Drawing.Size(50, 30)
+        Me.btnMultiplicar.TabIndex = 0
+        Me.btnMultiplicar.Text = "x"
+        Me.btnMultiplicar.UseVisualStyleBackColor = True
+        '
+        'btnDividir
+        '
+        Me.btnDividir.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDividir.Location = New System.Drawing.Point(221, 107)
+        Me.btnDividir.Name = "btnDividir"
+        Me.btnDividir.Size = New System.Drawing.Size(50, 30)
+        Me.btnDividir.TabIndex = 0
+        Me.btnDividir.Text = "÷"
+        Me.btnDividir.UseVisualStyleBackColor = True
+        '
+        'btnSubtrair
+        '
+        Me.btnSubtrair.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubtrair.Location = New System.Drawing.Point(221, 185)
+        Me.btnSubtrair.Name = "btnSubtrair"
+        Me.btnSubtrair.Size = New System.Drawing.Size(50, 30)
+        Me.btnSubtrair.TabIndex = 0
+        Me.btnSubtrair.Text = "-"
+        Me.btnSubtrair.UseVisualStyleBackColor = True
+        '
+        'btnSomar
+        '
+        Me.btnSomar.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSomar.Location = New System.Drawing.Point(221, 224)
+        Me.btnSomar.Name = "btnSomar"
+        Me.btnSomar.Size = New System.Drawing.Size(50, 30)
+        Me.btnSomar.TabIndex = 0
+        Me.btnSomar.Text = "+"
+        Me.btnSomar.UseVisualStyleBackColor = True
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Location = New System.Drawing.Point(12, 29)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.ReadOnly = True
+        Me.txtResultado.Size = New System.Drawing.Size(205, 20)
+        Me.txtResultado.TabIndex = 1
+        '
+        'btnIgual
+        '
+        Me.btnIgual.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIgual.Location = New System.Drawing.Point(150, 223)
+        Me.btnIgual.Name = "btnIgual"
+        Me.btnIgual.Size = New System.Drawing.Size(50, 30)
+        Me.btnIgual.TabIndex = 0
+        Me.btnIgual.Text = "="
+        Me.btnIgual.UseVisualStyleBackColor = True
+        '
+        'frmCalculadora
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(283, 266)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnSomar)
+        Me.Controls.Add(Me.btnPonto)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btnIgual)
+        Me.Controls.Add(Me.btnLimpar)
+        Me.Controls.Add(Me.btnSubtrair)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btnDividir)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btnMultiplicar)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Name = "frmCalculadora"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CALCULADORA"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btnLimpar As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnPonto As Button
+    Friend WithEvents btnMultiplicar As Button
+    Friend WithEvents btnDividir As Button
+    Friend WithEvents btnSubtrair As Button
+    Friend WithEvents btnSomar As Button
+    Friend WithEvents txtResultado As MaskedTextBox
+    Friend WithEvents btnIgual As Button
+End Class
---
+++ Calculadora/Form1.vb
@@ -0,0 +1,190 @@
+﻿Public Class frmCalculadora
+    Private resultado As Double = 0
+    Private numero As Double = 0
+    Private operador As String = String.Empty
+    Private strNumero As String = "1"
+    Private Limpar As Boolean = True
+    Private Ponto As Boolean = False
+    Private blnoperador As Boolean = False
+
+    Private Sub btnDividir_Click(sender As Object, e As EventArgs) Handles btnDividir.Click
+
+        If strNumero = "1" And blnoperador = True Then
+            resultado = Convert.ToDouble(txtResultado.Text)
+        ElseIf strNumero = "2" And blnoperador = True Then
+            resultado = Convert.ToDouble(txtResultado.Text)
+            resultado = resultado / numero
+            txtResultado.Text = numero
+            numero = 0
+        End If
+        operador = "/"
+        Limpar = True
+        Ponto = False
+        blnoperador = False
+    End Sub
+    Private Sub btnLimpar_Click(sender As Object, e As EventArgs) Handles btnLimpar.Click, btnIgual.Click
+        numero = 0
+        resultado = 0
+        operador = String.Empty
+        txtResultado.Text = ""
+        Limpar = True
+    End Sub
+    Private Sub btnMultiplicar_Click(sender As Object, e As EventArgs) Handles btnMultiplicar.Click
+        If strNumero = "1" And blnoperador = True Then
+            resultado = Convert.ToDouble(txtResultado.Text)
+
+        ElseIf strNumero = "2" And blnoperador = True Then
+            resultado = Convert.ToDouble(txtResultado.Text)
+            resultado = resultado * numero
+            txtResultado.Text = numero
+            numero = 0
+        End If
+        operador = "*"
+        Limpar = True
+        Ponto = False
+        blnoperador = False
+    End Sub
+    Private Sub btnSubtrair_Click(sender As Object, e As EventArgs) Handles btnSubtrair.Click
+        If strNumero = "1" And blnoperador = True Then
+
+
+            resultado = Convert.ToDouble(txtResultado.Text)
+
+        ElseIf strNumero = "2" And blnoperador = True Then
+            resultado = Convert.ToDouble(txtResultado.Text)
+            resultado = resultado - numero
+            txtResultado.Text = numero
+            numero = 0
+        End If
+        operador = "-"
+        Limpar = True
+        Ponto = False
+        blnoperador = False
+    End Sub
+    Private Sub btnSomar_Click(sender As Object, e As EventArgs) Handles btnSomar.Click
+        If strNumero = "1" And blnoperador = True Then
+            resultado = Convert.ToDouble(txtResultado.Text)
+
+        ElseIf strNumero = "2" And blnoperador = True Then
+            resultado = Convert.ToDouble(txtResultado.Text)
+            resultado = resultado + numero
+            txtResultado.Text = numero
+            numero = 0
+        End If
+        operador = "+"
+        Limpar = True
+        Ponto = False
+        blnoperador = False
+    End Sub
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        If Limpar Then
+            txtResultado.Text = "0"
+            Limpar = False
+        Else
+            txtResultado.Text = txtResultado.Text & "0"
+        End If
+        blnoperador = True
+    End Sub
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        If Limpar Then
+            txtResultado.Text = "1"
+            Limpar = False
+        Else
+            txtResultado.Text = txtResultado.Text & "1"
+        End If
+        blnoperador = True
+    End Sub
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        If Limpar Then
+            txtResultado.Text = "2"
+            Limpar = False
+        Else
+            txtResultado.Text = txtResultado.Text & "2"
+        End If
+        blnoperador = True
+    End Sub
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        If Limpar Then
+            txtResultado.Text = "3"
+            Limpar = False
+        Else
+            txtResultado.Text = txtResultado.Text & "3"
+        End If
+        blnoperador = True
+    End Sub
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        If Limpar Then
+            txtResultado.Text = "4"
+            Limpar = False
+        Else
+            txtResultado.Text = txtResultado.Text & "4"
+        End If
+        blnoperador = True
+    End Sub
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        If Limpar Then
+            txtResultado.Text = "5"
+            Limpar = False
+        Else
+            txtResultado.Text = txtResultado.Text & "5"
+        End If
+        blnoperador = True
+    End Sub
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        If Limpar Then
+            txtResultado.Text = "6"
+            Limpar = False
+        Else
+            txtResultado.Text = txtResultado.Text & "6"
+        End If
+        blnoperador = True
+    End Sub
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        If Limpar Then
+            txtResultado.Text = "7"
+            Limpar = False
+        Else
+            txtResultado.Text = txtResultado.Text & "7"
+        End If
+        blnoperador = True
+    End Sub
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        If Limpar Then
+            txtResultado.Text = "8"
+            Limpar = False
+        Else
+            txtResultado.Text = txtResultado.Text & "8"
+        End If
+        blnoperador = True
+    End Sub
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        If Limpar Then
+            txtResultado.Text = "9"
+            Limpar = False
+        Else
+            txtResultado.Text = txtResultado.Text & "9"
+        End If
+        blnoperador = True
+    End Sub
+    Private Sub btnPonto_Click(sender As Object, e As EventArgs) Handles btnPonto.Click
+        If Ponto = False Then
+            txtResultado.Text = txtResultado.Text & "."
+            Ponto = True
+        End If
+    End Sub
+
+    Private Sub btnIgual_Click(sender As Object, e As EventArgs) Handles btnPonto.Click
+        If operador = "+" And numero = 0 Then
+            resultado = resultado + Convert.ToDouble(txtResultado.Text)
+        ElseIf operador = "-" And numero = 0 Then
+            resultado = resultado - Convert.ToDouble(txtResultado.Text)
+        ElseIf operador = "*" And numero = 0 Then
+            resultado = resultado * Convert.ToDouble(txtResultado.Text)
+        ElseIf operador = "/" And numero = 0 Then
+            resultado = resultado / Convert.ToDouble(txtResultado.Text)
+        End If
+        txtResultado.Text = resultado
+        Limpar = True
+        blnoperador = False
+    End Sub
+End Class
